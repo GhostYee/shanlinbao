74df975c59d62d978ad61b0fd395443fd4155069
xionglihui <xionglihui@vip.qq.com>
2017-01-13 11:55:00 +0800

    首页
    首页

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,6 +1,9 @@
 // pages/main/main.js
 Page({
-  data:{},
+  data:{
+    imgUrls:["http://image.shanlinbao.com/bao/banner/2016/04/25/8ef2fbc1-dd27-43e0-856b-d09328c47ff6.png","http://image.shanlinbao.com/bao//banner/2017/01/10/b3263e6a-3c46-4db0-a6c0-cb42183d7cde.jpg","http://image.shanlinbao.com/bao//banner/2017/01/10/11308c24-efc0-41d2-a038-b8daa2bcd06c.png"],
+    
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
